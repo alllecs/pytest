59097571f28be8ecdd90c9b771298a30977f962c
Alexander Smirnov <alllecs@yandex.ru>
2021-03-10 15:02:17 +0300

    Added assert

--- test_items.py
+++ test_items.py
@@ -4,4 +4,6 @@ def test_check_add_to_basket(browser):
     browser.get(link)
     button = browser.find_element_by_class_name("btn-add-to-basket")
     button.click()
+    result = browser.find_element_by_class_name("alertiner").text
+    assert "Coders at Work" in result, print(result)
 #    browser.find_element_by_css_selector("#login_link")
