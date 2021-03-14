73b7d57cafbe84cf0d8437ec319d0c1dba27214a
Alexander Smirnov <alllecs@yandex.ru>
2021-03-14 13:01:06 +0300

    Fixed class_name for result add to basket

--- test_items.py
+++ test_items.py
@@ -1,9 +1,12 @@
+import time
+
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 def test_check_add_to_basket(browser):
     browser.get(link)
     button = browser.find_element_by_class_name("btn-add-to-basket")
     button.click()
-    result = browser.find_element_by_class_name("alertiner").text
+    time.sleep(5)
+    result = browser.find_element_by_class_name("alertinner ").text
     assert "Coders at Work" in result, print(result)
 #    browser.find_element_by_css_selector("#login_link")
