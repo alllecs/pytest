4e4d68f5fe70dff4b11d7074ef626f51d506a51c
Alexander Smirnov <alllecs@yandex.ru>
2021-03-14 14:15:16 +0300

    Added try-except for find error

--- test_items.py
+++ test_items.py
@@ -1,12 +1,23 @@
 import time
+from selenium.common.exceptions import NoSuchElementException
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" #work
+#link = "http://selenium1py.pythonanywhere.com/catalogue/hackers-painters_185/" #does not work
 
 def test_check_add_to_basket(browser):
     browser.get(link)
-    button = browser.find_element_by_class_name("btn-add-to-basket")
-    button.click()
-    time.sleep(5)
-    result = browser.find_element_by_class_name("alertinner ").text
-    assert "Coders at Work" in result, print(result)
-#    browser.find_element_by_css_selector("#login_link")
+    try:
+        button = browser.find_element_by_css_selector("#add_to_basket_form > .btn-add-to-basket")
+        button.click()
+    except NoSuchElementException:
+        assert False, print("Dont find button add to basket")
+
+    #else:
+        #button = browser.find_element_by_css_selector("#add_to_basket_form > .btn-add-to-basket")
+
+    # button = browser.find_element_by_css_selector("#add_to_basket_form > .btn-add-to-basket")
+    # button.click()
+    # time.sleep(5)
+    # assert browser.find_element_by_css_selector("#add_to_basket_form > .btn-add-to-basket") < 1
+    #result = browser.find_element_by_class_name("alertinner ").text
+    #assert "Coders at Work" in result, print("Not added to basket")
